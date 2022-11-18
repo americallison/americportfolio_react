08eb7c36ec57472004a3360f6a8dda5c859b95a8
Americ Allison <americ474@gmail.com>
2022-11-18 16:45:36 +0000

    added link to name

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -54,8 +54,8 @@ export default function MainNavBar () {
                   sm:items-stretch sm:justify-start
                 ">
               <div className="flex flex-shrink-0 items-center">
-                <h4 className="block h-6 w-auto lg:hidden">Americ Allison</h4>
-                <h4 className="hidden h-6 w-auto lg:block">Americ Allison</h4>
+                <h4 className="block h-6 w-auto lg:hidden"><a href="/">Americ Allison</a></h4>
+                <h4 className="hidden h-6 w-auto lg:block"><a href="/">Americ Allison</a></h4>
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                 {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
