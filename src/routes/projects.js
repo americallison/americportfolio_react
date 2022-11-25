4761d5c6ea51072f9c2e05ccedd2ca9ce2f0f54f
Americ Allison <americ474@gmail.com>
2022-11-25 14:43:43 +0000

    updated contents

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import MainNavBar from '../components/MainNavBar';
+import pic from "../components/images/Phonebook.png"
+import pic1 from "../components/images/portfolio.png"
 
 export default function Projects() {
     return (
@@ -8,7 +10,7 @@ export default function Projects() {
         <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
     {/* <!--Card 1--> */}
     <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="https://picsum.photos/id/25/100" alt="Mountain" />
+      <img src={pic} alt="Mountain" />
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">Phonebook in React</div>
         <p class="text-gray-700 text-base">
@@ -24,7 +26,7 @@ export default function Projects() {
     </div>
     {/* <!--Card 2--> */}
     <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="https://picsum.photos/id/30/100" alt="River" />
+      <img src={pic1} height={70} alt="River" />
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">My Portfolio</div>
         <p class="text-gray-700 text-base">
@@ -36,22 +38,6 @@ export default function Projects() {
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#summer</span>
-      </div>
-    </div>
-
-    {/* <!--Card 3--> */}
-    <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="https://picsum.photos/id/45/100" alt="Forest" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Forest</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fall</span>
       </div>
     </div>
   </div>
