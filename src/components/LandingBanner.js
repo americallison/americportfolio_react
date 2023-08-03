59cc21734a7f3e12c6ad438d059cd63058b33b8f
Americ Allison <americ474@gmail.com>
2023-08-03 19:04:48 +0000

    edited

--- src/components/LandingBanner.js
+++ src/components/LandingBanner.js
@@ -17,15 +17,11 @@ const LandingBanner = () => {
           <div className="md:w-6/12 w-full p-5 bmd:static bg-[#173F5F] bg-opacity-90
           bg-rounded fixed top-20 h-screen md:h-screen left-0 md:flex md:flex-col md:justify-center">
             <h3 className="text-center text-white text-3xl capitalize m-2 font-light">Americ Allison</h3>
-            <small className="text-center block text-2xl text-white mb-3">Web Developer, Teacher &amp; Network Technician</small>
+            <small className="text-center block text-xl text-white mb-3">
+              Aspiring Software Engineer Web Developer, Teacher &amp; Data Analyst</small>
             <p className="text-justify text-lg mt-2 p-3 m-2 text-[#F6D55C]">
               Hello, I am a tech enthusiast presently engaged in Computer
-              Programming and basic networking. I obtained a Bachelor of Science degree in Plant and Soil Science from Cuttington University and minored
-              in Accounting. I have been involved with technology for about 5 years. I first started writing small desktop programs in
-              Python using Tkinter and building small static websites using HTML and CSS. I then transitioned to web applications with Flask.
-              My major languages are HTML, CSS, Python,
-              Javascript. My major frameworks are Bootstrap, Tailwind CSS, Flask. I have experience in using two of the most popular
-              tools used in collaborative programming (Git & GitHub).
+              Education and Programming, Data Analysis and Basic Computer Networking. 
             </p>
             <button className="mt-5 bg-[#ED553B] hover:bg[#172F5F] text-white px-4 py-2 shadow">
               Get Started
@@ -35,7 +31,7 @@ const LandingBanner = () => {
 
         {/* Image Banner for Small Devices */}
         <div className="md:hidden absolute inset-0 bg-cover bg-center">
-          <img src={profile} alt="Banner" className="w-full h-full absolute inset-0 opacity-50" />
+          <img src={profile} alt="Banner" className="w-full h-full absolute inset-0 opacity-20" />
         </div>
       </div>
 
--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -24,16 +24,6 @@ export default function PersonalDetails({isActive, setIsActive}) {
         <img className="w-60 h-54 rounded-xl" src={profile} alt="JB" />
       </div>
       <div id="content" className="prose lg:prose-xl px-2">
-        <h3 className="text-center text-sky-900 text-2xl capitalize m-2 font-medium">Americ Allison</h3>
-        <small className="text-center block text-md text-sky-700 mb-3 text-gray-800">Web Developer, Teacher &amp; Network Technician</small>
-        <p className="text-justify text-lg mt-2 p-3 m-2 text-gray-800">Hello, I am a tech enthusiast presently engaged in Computer 
-        Programming and basic networking. I obtained a Bachelor of Science degree in Plant and Soil Science from Cuttington University and minored 
-        in Accounting. I have been involved with technology for about 5 years. I first started writing small desktop programs in
-        Python using Tkinter and building small static websites using HTML and CSS. I then transitioned to web applications with Flask. 
-        My major languages are HTML, CSS, Python, 
-        Javascript. My major frameworks are Bootstrap, Tailwind CSS, Flask. I have experience in using two of the most popular
-        tools used in collaborative programming (Git & GitHub).</p>
-
         <div className="p-6 max-w-full mx-auto">
             <ul className="flex justify-center items-center gap-4">
               <li className={`border border-zinc-500 p-4 hover:scale-105 transition duration-300 ease-in-out ${isActive.active && isActive.name === "education" && 'bg-[#f2e7d3] '}`}>
