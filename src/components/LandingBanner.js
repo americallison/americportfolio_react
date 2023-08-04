305c928dddb2ea29a621995cae4631390678983c
Americ Allison <americ474@gmail.com>
2023-08-04 16:52:10 +0000

    landing page edit

--- src/components/LandingBanner.js
+++ src/components/LandingBanner.js
@@ -1,39 +1,68 @@
 import React from 'react';
 import profile from "../components/images/profile.jpg";
-
+import { Link } from 'react-router-dom';
 
 const LandingBanner = () => {
   return (
     <>
 
-      <div className="relative min-h-screen">
-        <div className="flex md:h-screen">
-          {/* Image Banner for Medium to Large Devices */}
-          <div className="hidden md:w-6/12 md:flex md:items-center md:justify-center md:bg-cover md:bg-center">
-            <img src={profile} alt="Banner" className="w-full h-full absolute inset-0 opacity-100" />
-          </div>
-
-          {/* Text Container */}
-          <div className="md:w-5/12 w-full p-5 bmd:static bg-[#173F5F] bg-opacity-90
-          shadow fixed top-20 h-screen md:h-screen left-0 md:flex md:flex-col md:justify-center">
-            <h3 className="text-center text-white text-3xl capitalize m-2 font-light">Americ Allison</h3>
-            <small className="text-center block text-xl text-white mb-3">
-              Aspiring Software Engineer Web Developer, Teacher &amp; Data Analyst</small>
-            <p className="text-lg mt-2 p-3 m-2 text-[#F6D55C]">
-              Hello, I am a tech enthusiast presently engaged in Computer
-              Education and Programming, Data Analysis and Basic Computer Networking. 
-            </p>
-            <Link to="/cv"><button className="m-4 bg-[#ED553B] hover:bg[#172F5F] text-white p-3 font-light shadow-lg">
-              View my CV
-            </button></Link>
-          </div>
-        </div>
-
-        {/* Image Banner for Small Devices */}
-        <div className="md:hidden absolute inset-0 bg-cover bg-center">
-          <img src={profile} alt="Banner" className="w-full h-full absolute inset-0 opacity-20" />
-        </div>
+<div className="relative min-h-screen">
+  <div className="md:flex block">
+    {/* Text Container */}
+    <div className="md:w-6/12 w-full p-5 bg-[#173F5F] bg-opacity-90 flex flex-col justify-center">
+      <h3 className="text-[#f6d55c] text-5xl ml-5 md:mt-16 mt-5 mb-3 px-5 pt-3 font-bold">
+        Americ Allison
+      </h3>
+      <small className="block text-xl text-white px-5 ml-5 mt-1">
+        Aspiring Software Engineer Web Developer, Teacher &amp; Data Analyst
+      </small>
+      <p className="text-lg mb-3 px-5 pt-3 ml-5 mt-3 text-[#F6D55C]">
+        Hello, I am a tech enthusiast presently engaged in Computer
+        Education and Programming, Data Analysis and Basic Computer Networking.
+      </p>
+      <div className="m-5">
+        <Link to="/cv" className="m-5">
+          <button className="bg-[#ED553B] hover:bg-[#172F5F] text-xl text-white p-3 font-light shadow-lg
+          animate-fade-in-button delay-2000">
+            View my CV
+          </button>
+        </Link>
       </div>
+    </div>
+
+    {/* Image Banner */}
+    <div
+      className="md:w-6/12 shadow-lg rounded-sm w-full animate-fade-in-image delay-3000"
+      style={{
+        backgroundImage: `url(${profile})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'fit',
+      }}
+    ></div>
+  </div>
+
+  <div className="ml-5">
+    <p className="text-white p-5 ml-5 mt-4 text-xl">Check me out on:</p>
+  </div>
+
+  <div className="flex mt-3">
+    <div className="md:w-4/12 p-3 m-4">
+      <button className="text-white w-full text-xl bg-[#ed553b] font-light p-4 m-2">
+        LinkedIn
+      </button>
+    </div>
+    <div className="md:w-4/12 p-3 m-4">
+      <button className="text-white w-full text-xl bg-[#ed553b] font-light p-4 m-2">
+        GitHub
+      </button>
+    </div>
+    <div className="md:w-4/12 p-3 m-4">
+      <button className="text-white w-full text-xl bg-[#ed553b] font-light p-4 m-2">
+        Stack Overflow
+      </button>
+    </div>
+  </div>
+</div>;
 
     </>
   );
