a18557c7099e63c56a998eaaaaf85bb61df0c2cd
Americ Allison <americ474@gmail.com>
2023-08-09 17:04:48 +0000

    add brand icons

--- src/components/LandingBanner.js
+++ src/components/LandingBanner.js
@@ -2,7 +2,8 @@ import React from 'react';
 import profile from "../components/images/profile.jpg";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faNoteSticky } from '@fortawesome/free-solid-svg-icons';
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
+import { faGithubAlt, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
 
 
 const LandingBanner = () => {
@@ -11,52 +12,49 @@ const LandingBanner = () => {
 
 <div className="relative min-h-screen">
   <div className="md:flex block">
+     {/* Image Banner */}
+     <div
+      className="md:w-6/12 shadow-lg p-5 md:h-auto md:m-5 md:ml-12 md:mt-8 h-64 rounded-sm animate-fade-in-image delay-3000"
+      style={{
+        backgroundImage: `url(${profile})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'object',
+      }}
+    ></div>
     {/* Text Container */}
-    <div className="md:w-6/12 w-full p-5 bg-[#173F5F] bg-opacity-90 flex flex-col justify-center">
-      <h3 className="text-[#f6d55c] text-5xl md:ml-5 md:mt-16 m-2 md:mr-5 md:mb-3 ms:px-5 md:pt-3 font-bold">
+    <div className="md:w-6/12 w-full p-5 bg-opacity-90 flex flex-col justify-center">
+      <h3 className="text-[#031627] text-4xl md:ml-5 md:mt-5 m-2 md:mr-5 md:mb-3 ms:px-5 md:pt-3 font-bold">
         Americ Allison
       </h3>
-      <small className="text-xl text-white md:ml-5 sm:m-2 md:mt-1 mt-1">
+      <small className="text-xl md:ml-5 text-[#389F91] m-2 md:mt-1 mt-1">
         Aspiring Software Engineer Web Developer, Teacher &amp; Data Analyst
       </small>
-      <p className="text-lg m-2 md:mb-3 md:mb-2 md:pt-3 md:ml-5 md:mt-3 text-[#F6D55C]">
+      <p className="text-lg m-2 md:mb-3 text-stone-400 mb-2 md:pt-3 md:ml-5 md:mt-2">
         Hello, I am a tech enthusiast presently engaged in Computer
         Education and Programming, Data Analysis and Basic Computer Networking.
       </p>
       <div>
         <Link to="/cv" className="md:m-5 m-2">
-          <button className="bg-[#ED553B] hover:bg-[#172F5F] text-xl text-white p-3 font-light shadow-lg
-          animate-fade-in-button delay-2000"><FontAwesomeIcon icon={faNoteSticky} />&nbsp;
+          <button className="bg-[#EF6C01] hover:bg-[#172F5F] text-xl text-white p-3 font-light shadow-lg
+          animate-fade-in-button delay-2000"><FontAwesomeIcon icon={faBookOpen} />&nbsp;
             View my CV
           </button>
         </Link>
       </div>
     </div>
 
-    {/* Image Banner */}
-    <div
-      className="md:w-6/12 w-full shadow-lg md:h-auto h-64 rounded-sm animate-fade-in-image delay-3000"
-      style={{
-        backgroundImage: `url(${profile})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'fit',
-      }}
-    ></div>
-  </div>
-
-  <div className="ml-5">
-    <p className="text-white ml-5 mt-4 text-xl">Check me out on:</p>
+   
   </div>
 
   <div className="md:flex block md:mt-1 animate-fade-in-image delay-3000">
     <div className="md:w-4/12 w-full p-3 m-4">
       <button className="text-white w-full text-xl bg-[#ed553b] font-light p-4 m-2">
-      <FontAwesomeIcon icon={["fab", "github"]} />&nbsp;LinkedIn
+      <FontAwesomeIcon icon={faGithubAlt} />&nbsp;LinkedIn
       </button>
     </div>
     <div className="md:w-4/12 w-full p-3 m-4">
       <button className="text-white w-full text-xl bg-[#ed553b] font-light p-4 m-2">
-        GitHub
+      <FontAwesomeIcon icon={faGithubAlt} />&nbsp;GitHub
       </button>
     </div>
     <div className="md:w-4/12 p-3 w-full m-4">
--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -11,7 +11,7 @@ export default function MainNavBar () {
     };
   
     return (
-      <nav className="bg-[#173F5F] z-60">
+      <nav className="z-60">
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
               <div className="relative flex items-center justify-between h-16">
                   <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -69,35 +69,35 @@ export default function MainNavBar () {
                       {/* Logo */}
                       <div className="flex-shrink-0 flex items-center">
                           
-                          <h1 className='text-white logo-text'>&nbsp;<Link to="/">AMERIC ALLISON</Link></h1>
+                          <h1 className='text-[#252B48] font-bold logo-text'>&nbsp;<Link to="/">PABS TECH</Link></h1>
                       </div>
                       {/* Navigation links */}
                       <div className="hidden justify-end sm:block sm:ml-6">
                           <div className="flex space-x-4">
-                              <Link to="/" className="text-[#f6d55c] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                              <Link to="/" className="text-[#389F91] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                               >
                                   Home
                               </Link>
-                              <Link to="/cv" className="text-[#f6d55c] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                              <Link to="/cv" className="text-[#389F91] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                               >
                                   Curriculum Vitae
                               </Link>
-                              <Link to="/projects" className="text-[#f6d55c] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                              <Link to="/projects" className="text-[#389F91] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                               >
                                               Projects
                               </Link>
                               <Link to="/contact"
-                                  className="text-[#f6d55c] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                  className="text-[#389F91] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                               >
                                  Contact
                               </Link>
                               <Link to="/skills"
-                                  className="text-[#f6d55c] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                  className="text-[#389F91] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                               >
                                  Skills
                               </Link>
                               <Link to="/about"
-                                  className="text-[#f6d55c] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                  className="text-[#389F91] hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                               >
                                  About Me
                               </Link>
@@ -107,14 +107,14 @@ export default function MainNavBar () {
 </div>
 <div>
                   {menuShow ? (
-                      <div className="absolute inset-top-[12] inset-left-[2] bg-black bg-opacity-50 z-40 p-2"> 
+                      <div className="absolute inset-top-[12] inset-left-[2] bg- z-40 p-5"> 
                       <ul>
-                          <li className='md:hidden p-1 text-[#f6d55c]'><Link to="/">Home</Link></li>
-                          <li className='md:hidden p-1 text-[#f6d55c]'><Link to="/cv">CV</Link></li>
-                          <li className="md:hidden p-1 text-[#f6d55c]"><Link to="/projects">Projects</Link></li>
-                          <li className='md:hidden p-1 text-[#f6d55c]'><a href="/contact">Contact</a></li>
-                          <li className='md:hidden p-1 text-[#f6d55c]'><a href="/skills">Skills</a></li>
-                          <li className='md:hidden p-1 text-[#f6d55c]'><a href="/about">About Me</a></li>
+                          <li className='md:hidden p-1 text-[#389F91]'><Link to="/">Home</Link></li>
+                          <li className='md:hidden p-1 text-[#389F91]'><Link to="/cv">CV</Link></li>
+                          <li className="md:hidden p-1 text-[#389F91]"><Link to="/projects">Projects</Link></li>
+                          <li className="md:hidden p-1 text-[#389F91]"><Link to="/contact">Contact</Link></li>
+                          <li className='md:hidden p-1 text-[#f6d55c]'><Link to="/skills">Skills</Link></li>
+                          <li className='md:hidden p-1 text-[#f6d55c]'><Link to="/about">About Me</Link></li>
                                 </ul>
                       </div>
                   ): ("")}
